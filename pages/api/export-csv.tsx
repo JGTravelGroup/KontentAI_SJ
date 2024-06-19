399ee5131d8c4f41e98f1291b454591f438c91c9
JGTravelGroup <163517645+JGTravelGroup@users.noreply.github.com>
2024-06-19 14:39:55 +0100

    Update export-csv.tsx

--- pages/api/export-csv.tsx
+++ pages/api/export-csv.tsx
@@ -104,7 +104,7 @@ export default async function handler(req, res) {
     res.status(200).send(csv);
   } catch (err) {
     console.log(err)
-    res.status(500).json({ error: 'Failed to generate CSV file' });
+    res.status(500).json({ error: 'Failed to generate CSV file' . err });
   }
 }
 
