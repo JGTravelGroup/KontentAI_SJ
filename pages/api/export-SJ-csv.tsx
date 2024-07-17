6c81d68133cf32f819aeb93eac6f341091082365
JGTravelGroup <163517645+JGTravelGroup@users.noreply.github.com>
2024-07-17 14:41:01 +0100

    Update export-SJ-csv.tsx

--- pages/api/export-SJ-csv.tsx
+++ pages/api/export-SJ-csv.tsx
@@ -45,7 +45,7 @@ export default async function handler(req, res) {
     res.status(200).send(csv);
   } catch (err) {
     console.log(err)
-    res.status(500).json({ error: 'Failed to generate single-tour SJ CSV file' . err });
+    res.status(500).json({ error: 'Failed to generate single-tour SJ CSV file' });
   }
 }
 
