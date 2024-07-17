179b2f401556eeb43821cd012b17b184d0296ff0
JGTravelGroup <163517645+JGTravelGroup@users.noreply.github.com>
2024-07-17 13:55:04 +0100

    Update export-SJ.tsx

--- components/custom-elements/export-SJ.tsx
+++ components/custom-elements/export-SJ.tsx
@@ -8,7 +8,7 @@ interface IProps {
     value: string | null | object | CustomElement.ValueObject | any
 }
 
-export const ExportCustomElement: React.FC<IProps> = ({ element, context, value, handleSave }) => {
+export const ExportCustomElement_SJ: React.FC<IProps> = ({ element, context, value, handleSave }) => {
     return <>
         <div className="custom-element">
             <div className="d-flex justify-content-center mb-3 p-1 py-5">
